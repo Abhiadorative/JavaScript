49a89b28e90c5ed84aa80274b4eb46080e1eae5f
Abhi Lodhi <131201212+Abhiadorative@users.noreply.github.com>
2023-10-31 12:03:17 +0900

    variables.js

---
+++ Basic_js/variables.js
@@ -0,0 +1,14 @@
+const accountId=  14441
+let accountEmail= "abhilodhi@google.com"
+var accoutPassword= "12345"
+accountCity= "Nagpur"
+let accountState;
+
+//accountId = 2 // not allowed
+console.log (accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/ 
+
+console.table([accountId, accountEmail, accoutPassword, accountCity , accountState]);
