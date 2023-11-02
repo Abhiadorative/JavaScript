f82315ce494ec9aa243cdcf5163a3476d4d44794
Abhi Lodhi <131201212+Abhiadorative@users.noreply.github.com>
2023-11-02 11:59:48 +0900

    Conevertionoperation.js

---
+++ Basic_js/conevertionoperation.js
@@ -0,0 +1,85 @@
+let score = 33
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+let score = undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+let score = true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber = Number (score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);*/
+
+let score="abhi"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntnumber= Number(score)
+console.log(typeof valueIntnumber);
+console.log(valueIntnumber);
+
+// " 33 " => 33
+//" 33abc " => NaN (but type is number)
+//true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+ let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let isLoggedIn = "Abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" =>  false; 
+// "Abhi" => true
+
+let someNumber =  45
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
