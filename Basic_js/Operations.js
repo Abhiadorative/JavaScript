232d44dc2d425e359a3720903cdb48be08c5c3b5
Abhi Lodhi <131201212+Abhiadorative@users.noreply.github.com>
2023-11-03 11:06:45 +0900

    Operations.js

---
+++ Basic_js/Operations.js
@@ -0,0 +1,38 @@
+//******************************_Operations_************************************** 
+
+Here we getting Negative Values
+let value = 3
+let negValue = -value
+ console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);//power
+console.log(2/3);
+console.log(2% 7);
+
+let str1="Hello_Abhi"
+let str2 =" Dear"
+
+let str3=str1+str2;
+
+console.log(str3);
+console.log("1" + 2);
+console.log(1+ "2" +2);
+console.log(1+2 +"2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 3
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
